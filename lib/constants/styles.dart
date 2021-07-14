3ac9127462092cfb035cb94c61f48b422f8b63f6
M Dimas Faizin <faizfiaz@yahoo.co.id>
2021-07-14 16:00:46 +0700

    reverting

--- lib/constants/styles.dart
+++ lib/constants/styles.dart
@@ -7,32 +7,32 @@ class BaseStyle {
   static const textBold28 = TextStyle(fontWeight: FontWeight.bold, fontSize: 28, color: primaryText);
   static const textRegular16 = TextStyle(fontWeight: FontWeight.w400, fontSize: 16, color: primaryText);
 
-  static textFieldIconDefault (IconData iconData, String image, text) {
-    InputDecoration(
-        contentPadding:
-        const EdgeInsets.symmetric(vertical: 14.0),
-        prefixIcon: Container(
-          margin: EdgeInsets.only(
-              right: 16, left: 16, top: 4, bottom: 4),
-          padding: EdgeInsets.only(right: 10),
-          child: Icon(
-            Icons.email_rounded,
-            color: primary,
-            size: 18,
-          ),
-        ),
-        focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: secondary, width: 0.5)),
-        enabledBorder: OutlineInputBorder(
-            borderSide:
-            BorderSide(color: primaryText, width: 0.5)),
-        border: OutlineInputBorder(
-            borderSide:
-            BorderSide(color: primaryText, width: 0.5)),
-        hintText: "Email",
-        errorText: _viewModel.errorEmail
-            ? txt("email_not_valid")
-            : null);
-  }
+  // static textFieldIconDefault (IconData iconData, String image, text) {
+  //   InputDecoration(
+  //       contentPadding:
+  //       const EdgeInsets.symmetric(vertical: 14.0),
+  //       prefixIcon: Container(
+  //         margin: EdgeInsets.only(
+  //             right: 16, left: 16, top: 4, bottom: 4),
+  //         padding: EdgeInsets.only(right: 10),
+  //         child: Icon(
+  //           Icons.email_rounded,
+  //           color: primary,
+  //           size: 18,
+  //         ),
+  //       ),
+  //       focusedBorder: OutlineInputBorder(
+  //           borderSide: BorderSide(color: secondary, width: 0.5)),
+  //       enabledBorder: OutlineInputBorder(
+  //           borderSide:
+  //           BorderSide(color: primaryText, width: 0.5)),
+  //       border: OutlineInputBorder(
+  //           borderSide:
+  //           BorderSide(color: primaryText, width: 0.5)),
+  //       hintText: "Email",
+  //       errorText: _viewModel.errorEmail
+  //           ? txt("email_not_valid")
+  //           : null);
+  // }
 
 }
--- lib/ui/pages/login/login_screen.dart
+++ lib/ui/pages/login/login_screen.dart
@@ -141,7 +141,31 @@ class _LoginScreen extends BaseStateWidget<LoginScreen>
                 ),
                 TextField(
                   style: TextStyle(fontSize: 12),
-                  decoration: ,
+                  decoration: InputDecoration(
+                      contentPadding:
+                          const EdgeInsets.symmetric(vertical: 14.0),
+                      prefixIcon: Container(
+                        margin: EdgeInsets.only(
+                            right: 16, left: 16, top: 4, bottom: 4),
+                        padding: EdgeInsets.only(right: 10),
+                        child: Icon(
+                          Icons.email_rounded,
+                          color: primary,
+                          size: 18,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: secondary, width: 0.5)),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: primaryText, width: 0.5)),
+                      border: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: primaryText, width: 0.5)),
+                      hintText: "Email",
+                      errorText: _viewModel.errorEmail
+                          ? txt("email_not_valid")
+                          : null),
                   controller: _viewModel.controllerEmail,
                 ),
                 SizedBox(
